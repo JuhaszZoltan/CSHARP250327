4acf623c9d229b724768c0be281c6db9b56e14a1
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-03-27 13:42:17 +0100

    Add project files.

---
+++ FilmekCLI/Film.cs
@@ -0,0 +1,28 @@
+﻿namespace FilmekCLI;
+
+class Film
+{
+    public string Cim { get; set; }
+    public int Bemutato { get; set; }
+    public string Mufaj { get; set; }
+    public string Forgalmazo { get; set; }
+    public int Hossz { get; set; }
+
+    public override string ToString() =>
+        $"\t Cím: {Cim}\n" +
+        $"\t Bemutató éve: {Bemutato}\n" +
+        $"\t Műfaj: {Mufaj}\n" +
+        $"\t Gyártó/Forgamazó: {Forgalmazo}\n" +
+        $"\t Hossza: {Hossz} perc";
+
+    public Film(string beolvasottSor)
+    {
+        var tmp = beolvasottSor.Split(';');
+
+        Cim = tmp[0];
+        Bemutato = int.Parse(tmp[1]);
+        Mufaj = tmp[2];
+        Forgalmazo = tmp[3];
+        Hossz = int.Parse(tmp[4]);
+    }
+}
---
+++ FilmekCLI/Program.cs
@@ -0,0 +1,41 @@
+﻿using FilmekCLI;
+using System.Text;
+
+const string AdatokCSV = "D:\\PROJECTS\\CSHARP250327\\FilmekCLI\\RESOURCES\\filmekCLI_adatok.csv";
+
+const string KeresettmufajTXT = "D:\\PROJECTS\\CSHARP250327\\FilmekCLI\\RESOURCES\\keresettmufaj.txt";
+
+List<Film> filmek = [];
+
+using StreamReader sr = new(AdatokCSV, Encoding.UTF8);
+
+while (!sr.EndOfStream) filmek.Add(new(sr.ReadLine()));
+
+Console.WriteLine("3. feladat: " +
+    $"A tárhelyen {filmek.Count} film van.");
+
+var f4 = filmek.GroupBy(f => f.Forgalmazo);
+Console.WriteLine("4. feladat: gyártónként a filmek száma:");
+foreach (var grp in f4)
+    Console.WriteLine($"\t {grp.Key}: {grp.Count()} db");
+
+Console.Write("5. feladat: " +
+    "Adja meg egy műfaj nevét: ");
+string f5 = Console.ReadLine().ToLower();
+var f6 = filmek.Where(f => f.Mufaj == f5.ToLower());
+using StreamWriter sw = new(KeresettmufajTXT, append: false, Encoding.UTF8);
+if (!f6.Any())
+    sw.WriteLine($"Nincs a tárhelyen {f5} műfajú film!");
+else
+{
+    foreach (var film in f6)
+        sw.WriteLine($"{film.Cim}\t{film.Hossz}");
+}
+Console.WriteLine("6. feladat: a keresettmufaj.txt elkészült!");
+
+var f7 = filmek.MinBy(f => f.Bemutato);
+if (f7 is not null)
+{
+    Console.WriteLine($"7. feladat: a legkorábban bemutatott film a tárhelyen:");
+    Console.WriteLine(f7);
+}
